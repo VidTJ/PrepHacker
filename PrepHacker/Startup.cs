f7cc44783e1aa5d99c58478bf6f5957f3c59c416
VidTJ <vidyar.tj@gmail.com>
2016-11-28 21:08:23 -0500

    Add project files.

---
+++ PrepHacker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrepHacker.Startup))]
+namespace PrepHacker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
